d2cdaea59f3f421e7abecc7534a5249ec72dc804
pedro16797 <62699562+pedro16797@users.noreply.github.com>
2026-04-08 20:12:14 +0200

    Tweaks to layout
    Removed hardcoded raise layer and set it to reference the lower layer.

--- keyboards/sofle_choc/keymaps/vial/keymap.c
+++ keyboards/sofle_choc/keymaps/vial/keymap.c
@@ -22,30 +22,22 @@ enum layers {
     _ADJUST
 };
 
-/*
- * Note on Vial Dynamic Features:
- * VIAL_TAP_DANCE_ENTRIES and VIAL_COMBO_ENTRIES are defined in config.h.
- * The Vial core automatically manages the tap_dance_actions[] and key_combos[] arrays.
- * Hardcoding these arrays in this file would cause "multiple definition" linker errors.
- * TD(0) and TD(1) in the keymap are placeholders for these dynamic entries.
- */
-
 const uint16_t PROGMEM keymaps[4][MATRIX_ROWS][MATRIX_COLS] = {
 [_QWERTY] = LAYOUT(
     // Row 0
-    KC_ESC,   RALT(KC_1), RALT(KC_2), RALT(KC_3), LSFT(KC_6), RALT(KC_E),                       KC_MPLY, LSFT(KC_MINUS), LSFT(KC_1), KC_PSLS, LSFT(KC_NUBS), KC_NUBS,
+    KC_ESC,   RALT(KC_1), RALT(KC_2), RALT(KC_3), LSFT(KC_6), RALT(KC_E),                     KC_NUBS,    LSFT(KC_NUBS), KC_PSLS, LSFT(KC_1), LSFT(KC_MINUS), KC_MPLY,
     // Row 1
-    KC_TAB,   KC_Q,       KC_W,       KC_E,       KC_R,       KC_T,                             KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
+    KC_TAB,   KC_Q,       KC_W,       KC_E,       KC_R,       KC_T,                           KC_Y,       KC_U,       KC_I,       KC_O,       KC_P,       KC_BSPC,
     // Row 2
-    KC_CAPS,  KC_A,       KC_S,       KC_D,       KC_F,       KC_G,                             KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
+    KC_CAPS,  KC_A,       KC_S,       KC_D,       KC_F,       KC_G,                           KC_H,       KC_J,       KC_K,       KC_L,       KC_SCLN,    KC_ENT,
     // Row 3 (Left Row 3, Center L Btn, Center R Btn, Right Row 3)
-    KC_LSFT,  KC_Z,       KC_X,       KC_C,       KC_V,       KC_B,    KC_MUTE,   RGB_TOG,      KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_QUOT,
+    KC_LSFT,  KC_Z,       KC_X,       KC_C,       KC_V,       KC_B,    KC_MUTE,   RGB_TOG,    KC_N,       KC_M,       KC_COMM,    KC_DOT,     KC_SLSH,    KC_QUOT,
     // Thumb Row (Left 1-5, Right 1-5)
-                      KC_LGUI, KC_LALT, KC_LCTL, KC_SPACE, MO(_LOWER),   MO(_RAISE), KC_SPACE, KC_DEL, KC_PGUP, TD(1)
+                    KC_LGUI, KC_LALT, KC_LCTL, KC_SPACE, MO(_LOWER),                         MO(_RAISE), KC_SPACE, KC_DEL, KC_PGUP, TD(1)
 ),
 [_LOWER] = LAYOUT(
     // Row 0
-    KC_TRNS,  LSFT(KC_3), KC_GRV,     RALT(KC_4), RALT(KC_6), LSFT(KC_4),                       KC_TRNS, LSFT(KC_EQL), KC_EQL, RALT(KC_GRV), RALT(KC_RBRC), RALT(KC_LBRC),
+    KC_TRNS,  LSFT(KC_3), KC_GRV,     RALT(KC_4), RALT(KC_6), LSFT(KC_4),                       RALT(KC_LBRC), RALT(KC_RBRC), RALT(KC_GRV), KC_EQL, LSFT(KC_EQL), KC_TRNS,
     // Row 1
     LALT(KC_TAB), KC_KP_1, KC_KP_2,   KC_KP_3,    KC_PAST,    KC_PSLS,                          LSFT(KC_8), LSFT(KC_9), KC_UP, LSFT(KC_2), KC_MINUS, KC_TRNS,
     // Row 2
@@ -55,18 +47,7 @@ const uint16_t PROGMEM keymaps[4][MATRIX_ROWS][MATRIX_COLS] = {
     // Thumb Row
                       KC_KP_0, LSFT(KC_5), LSFT(KC_LBRC), KC_ENT, KC_TRNS,   KC_TRNS, KC_TRNS, KC_INS, KC_PGDN, KC_TRNS
 ),
-[_RAISE] = LAYOUT(
-    // Row 0
-    KC_TRNS,  LSFT(KC_3), KC_GRV,     RALT(KC_4), RALT(KC_6), LSFT(KC_4),                       KC_TRNS, LSFT(KC_EQL), KC_EQL, RALT(KC_GRV), RALT(KC_RBRC), RALT(KC_LBRC),
-    // Row 1
-    LALT(KC_TAB), KC_KP_1, KC_KP_2,   KC_KP_3,    KC_PAST,    KC_PSLS,                          LSFT(KC_8), LSFT(KC_9), KC_UP, LSFT(KC_2), KC_MINUS, KC_TRNS,
-    // Row 2
-    KC_TRNS,  KC_KP_4,    KC_KP_5,    KC_KP_6,    KC_PPLS,    KC_PMNS,                          KC_HOME, KC_LEFT, KC_DOWN, KC_RGHT, KC_END, KC_TRNS,
-    // Row 3
-    KC_RSFT,  KC_KP_7,    KC_KP_8,    KC_KP_9,    KC_PDOT,    LSFT(KC_0), KC_TRNS,   KC_TRNS,   RALT(KC_QUOT), RALT(KC_BSLS), LSFT(KC_COMM), LSFT(KC_DOT), LSFT(KC_SLSH), KC_LBRC,
-    // Thumb Row
-                      KC_KP_0, LSFT(KC_5), LSFT(KC_LBRC), KC_ENT, KC_TRNS,   KC_TRNS, KC_TRNS, KC_INS, KC_PGDN, KC_TRNS
-),
+[_RAISE] = [_LOWER],
 [_ADJUST] = LAYOUT(
     // Row 0
     KC_TRNS,  KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,                          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
